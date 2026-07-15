5a4f628db45ec851c37ab285a17c7a3a2d7ec69d
Kacper <k.wladrz@gmail.com>
2026-07-15 20:09:32 +0200

    fix

--- src/components/FadeIn/FadeIn.tsx
+++ src/components/FadeIn/FadeIn.tsx
@@ -1,26 +1,29 @@
-import {
-    cloneElement,
-    isValidElement,
-    useState,
-    type CSSProperties,
-} from "react";
-import type { FadeInProps } from "./FadeIn.types";
+import { type ReactNode, type CSSProperties, HTMLAttributes } from "react";
+import { useState } from "react";
 import useMountAfterRender from "../../hooks/useMountAfterRender";
+
+interface FadeInProps extends HTMLAttributes<HTMLDivElement> {
+    children: ReactNode;
+    settings?: { duration?: number; delay?: number };
+    initialOpacity?: number;
+    finalOpacity?: number;
+}
 
 const FadeIn = ({
     children,
     settings,
     initialOpacity = 0,
     finalOpacity = 1,
+    ...props
 }: FadeInProps) => {
     const [isVisible, setIsVisible] = useState(false);
-    const { duration, delay } = { duration: 500, delay: 0, ...settings };
+    const { duration = 500, delay = 0 } = settings ?? {};
 
     useMountAfterRender(() => {
         setIsVisible(true);
     });
 
-    const fadeInStyle: CSSProperties = {
+    const style: CSSProperties = {
         transitionProperty: "opacity",
         transitionDuration: `${duration}ms`,
         transitionDelay: `${delay}ms`,
@@ -28,16 +31,11 @@ const FadeIn = ({
         opacity: isVisible ? finalOpacity : initialOpacity,
     };
 
-    if (!isValidElement<{ style?: CSSProperties }>(children)) {
-        return children;
-    }
-
-    return cloneElement(children, {
-        style: {
-            ...children.props.style,
-            ...fadeInStyle,
-        },
-    });
+    return (
+        <div {...props} style={{ ...props.style, ...style }}>
+            {children}
+        </div>
+    );
 };
 
 FadeIn.displayName = "FadeIn";
